28cb5182085832d7d83878b7e2742f8fcac9921a
mjdamgaard <43005772+mjdamgaard@users.noreply.github.com>
2023-02-24 13:36:29 +0100

    Add files via upload

--- backup/insert_basic_entities_in_pub_sdb.sql
+++ backup/insert_basic_entities_in_pub_sdb.sql
@@ -16,139 +16,133 @@ INSERT INTO Bots () VALUES ();
 
 /* Inserting the most fundamental relations */
 
--- define the string fields used in '.Full lexical item='.
+-- define the string fields used in '.full lexical item='.
 
 INSERT INTO Strings (str)
 VALUES (
     "can be referenced by the full (non-abbreviated) lexical item given by"
 );
-SET @fullLexItem_has_full_lex = 1;
+SET @Str_canbereferencedbyfulllex = 1 + 0x0A00000000000000;
 
 INSERT INTO Strings (str)
-VALUES (".Full lexical item=");
-SET @abbrLexItem_has_full_lex = 2;
+VALUES (".full lexical item=");
+SET @Str_dotfulllex = 2 + 0x0A00000000000000;
 
 
 
 
-INSERT INTO Strings (str)
+INSERT INTO Texts (str)
 VALUES (
     CONCAT (
-        "This relation states about its Subject and its Object, ",
-        "the latter of which should be a text string that is part of an ",
-        "English sentence with a meaning attached to it ",
-        "(i.e. a lexical item), that the following is true: ",
-        "The Object (a string) forms a lexical item that can be seen ",
-        "as defining the Subject.\n",
-        -- "\n",
-        -- "For instance, if the Subject can be referenced by a noun, then the ",
-        -- "Object could be a string forming that noun.",
-        -- "And if the Subject is a relation that can be referenced by a verb, ",
-        -- "then the Object could be a string forming that verb.\n",
-        -- "\n",
-        -- "A special example of the latter case is if the Subject is this very ",
-        -- "relation descibed by this description. In that case, the Object ",
-        -- "could be the string: \'can be referenced by the lexical item given ",
-        -- "by\'.\n",
-        -- -- I am choosing to let periods and commas be outside of the (single)
-        -- -- quotation marks when it references a specific string or character.
-        -- "\n",
-        -- "As a standard, we propose that when the lexical items are nouns, ",
-        -- "they should be capitalized (in a manner fitting to that noun). ",
-        -- "(Note that nouns do not have to be single words; they can be ",
-        -- "compound nouns as well.)"
-        ""
+"This relation states about its Subject and its Object, ",
+"the latter of which should be a text string that is part of an ",
+"English sentence with a meaning attached to it ",
+"(i.e. a lexical item), that the following is true: ",
+"The Object (a string) forms a lexical item that can be seen ",
+"as defining the Subject.\n",
+"\t",
+"For instance, if the Subject can be referenced by a noun, then the ",
+"Object could be a string forming that noun.",
+"And if the Subject is a relation that can be referenced by a verb, ",
+"then the Object could be a string forming that verb.\n",
+"\t",
+"A special example of the latter case is if the Subject is this very ",
+"relation descibed by this description. In that case, the Object ",
+"could be the string: \'can be referenced by the lexical item given ",
+"by\'.\n",
+-- I am choosing to let periods and commas be outside of the (single)
+-- quotation marks when it references a specific string or character.
+"\t",
+"As a standard, we propose that first letter are not capitalized, ",
+"except if the word is part of a proper noun or an abbreviation that ",
+"requires capitalization."
     )
 );
-SET @description_has_full_lex = 3;
+SET @Text_description_of_rel_fulllex = 1 + 0x0B00000000000000;
 
 
 -- insert the '.Full lexical item=' fundamental relation.
-INSERT INTO SimpleTerms (
-    full_lexical_item,
-    abbr_lexical_item,
-    description
-)
-VALUES (
-    @fullLexItem_has_full_lex,
-    @abbrLexItem_has_full_lex,
-    @description_has_full_lex
-);
-SET @rel_has_full_lex = 1;
+-- INSERT INTO SimpleTerms (
+--     full_lexical_item,
+--     abbr_lexical_item,
+--     description
+-- )
+-- VALUES (
+--     @Str_canbereferencedbyfulllex,
+--     @Str_dotfulllex,
+--     @Str_description_of_rel_fulllex
+-- );
+SET @Term_rel_hasfulllex = 1 + 0x0200000000000000;
 
 
--- define the string fields used in '.Lexical item='.
+-- define the string fields used in '.lexical item='.
 INSERT INTO Strings (str)
 VALUES (
     "can be referenced by the abbreviated lexical item given by"
 );
-SET @fullLexItem_has_abbr_lex = 4;
+SET @Str_canbereferencedbyabbrlex = 3 + 0x0A00000000000000;
 
 INSERT INTO Strings (str)
-VALUES (".Lexical item=");
-SET @abbrLexItem_has_abbr_lex = 5;
+VALUES (".lexical item=");
+SET @Str_dotlexitem = 4 + 0x0A00000000000000;
 
 
-INSERT INTO Strings (str)
+INSERT INTO Texts (str)
 VALUES (
     CONCAT (
-        "This relation is similar to the relation with lexical item given by ",
-        "\'can be referenced by the full (non-abbreviated) ",
-        "lexical item given by\' ",
-        "(and abbreviated lexical item given by \'.Full lexical item=\'), ",
-        "except that the Object is this relation has to be the abbreviated ",
-        "lexical item rather than the full lexical item.\n",
-        -- "We propose the following standard for constructing ",
-        -- "abbreviated lexical items:\n",
-        -- " 1) Refer to abbreviated lexical items simply as ",
-        --   "\t\'lexical items.\'\n",
-        -- " 2) When referencing a relation, first of all reformulate the ",
-        --   "\tfull lexical item as a sentence beginning with \'has a/an\' ",
-        --   "\tand ending with \'given by\'. ",
-        --   "\tThen replace \'has a/an\' with \'.\' and \'given by\' with ",
-        --   "\teither \'=\' or \':\', remove any spaces before and after these ",
-        --   "\treplacements, and capitalize the first letter after \'.\' ",
-        --   "\tin the same fashion as for a section title in a text ",
-        --   "\t(and indeed the sentence should now look like a section title). ",
-        --   "\tUse \'=\' when expecting that users will generally only be "
-        --   "\tinterested in querying about one Object ",
-        --   "\t(i.e. the most fitting one) for the relation ",
-        --   "\t(given a specific Subject). ",
-        --   "\tAnd use \':\' when expecting that users will generally ",
-        --   "\toften be interested in querying for several Objects fitting the ",
-        --   "\trelation (given a specific Subject).\n",
-        -- " 3) When referencing a predicate, consider first of all if it ",
-        --   "\tis not possible to instead formulate that predicate via a ",
-        --   "\trelation instead. ",
-        --   "\tAnd if this is difficult in nature, simply abbreviate the full ",
-        --   "\tlexical item by simplifying it (if possible) ",
-        --   "\tas a shortened verb. ",
-        --   "\t(Note that verbs do not have to be single words, ",
-        --   "\tthough; they can be compound verbs as well.) ",
-        --   "\tDo not capitalize the first letter if the lexical item ",
-        --   "\tbegins with a verb.\n",
-        -- " 4) When referencing things, simply choose a fitting ",
-        --   "\t(possibly proper) noun and capitalize it in a fitting manner. ",
-        --   "\tFor improper nouns, include an uncapitalized ",
-        --   "\t\'a\' or \'an\' at the beginning."
-        ""
+"This relation is similar to the relation with lexical item given by ",
+"\'can be referenced by the full (non-abbreviated) ",
+"lexical item given by\' ",
+"(and abbreviated lexical item given by \'.full lexical item=\'), ",
+"except that the Object is this relation has to be the abbreviated ",
+"lexical item rather than the full lexical item.\n",
+"We propose the following standard for constructing ",
+"abbreviated lexical items.\n\t",
+-- TODO: I can write this shorter writing out the syntax instead..
+"1) First letters should not be capitalized, except if the given word is part ",
+"of a proper noun or an abbreviation that requires capitalization.\n\t",
+"2) Refer to abbreviated lexical items simply as ",
+"\'lexical items\' for short.\n\t",
+" 3) When referencing a relation, first of all reformulate the ",
+"full lexical item as a sentence beginning with \'has a/an\' ",
+"and ending with \'given by\'. ",
+"Then replace \'has a/an\' with \'.\' and \'given by\' with ",
+"\'=\', and remove any whitespaces before and after these replacements. ",
+-- "Use \'=\' when expecting that users will generally only be "
+-- "interested in querying about one Object ",
+-- "(i.e. the most fitting one) for the relation ",
+-- "(given a specific Subject). ",
+-- "And use \':\' when expecting that users will generally ",
+-- "often be interested in querying for several Objects fitting the ",
+-- "relation (given a specific Subject).\n",
+-- " 3) When referencing a predicate, consider first of all if it ",
+-- "is not possible to instead formulate that predicate via a ",
+-- "relation instead. ",
+"4) When referencing a predicate, simply abbreviate the full ",
+"lexical item by simplifying it (if possible) ",
+"as a shortened verb. ",
+"(Note that verbs do not have to be single words, ",
+"though; they can be compound verbs as well.) ",
+"5) When referencing things, simply choose a fitting ",
+"(possibly proper) noun. ",
+"Do not include \'a\' or \'an\' at the beginning."
     )
 );
-SET @description_has_abbr_lex = 6;
+SET @Text_description_of_haslexitem = 2 + 0x0B00000000000000;
 
 
 -- insert the '.Lexical item=' fundamental relation.
-INSERT INTO SimpleTerms (
-    full_lexical_item,
-    abbr_lexical_item,
-    description
-)
-VALUES (
-    @fullLexItem_has_abbr_lex,
-    @abbrLexItem_has_abbr_lex,
-    @description_has_abbr_lex
-);
-SET @rel_has_abbr_lex = 2;
+-- INSERT INTO SimpleTerms (
+--     full_lexical_item,
+--     abbr_lexical_item,
+--     description
+-- )
+-- VALUES (
+--     @Str_canbereferencedbyabbrlex,
+--     @Str_dotlexitem,
+--     @Str_description_of_haslexitem
+-- );
+SET @Term_rel_haslex = 2 + 0x0200000000000000;
 
 
 
@@ -156,35 +150,35 @@ SET @rel_has_abbr_lex = 2;
 
 INSERT INTO Strings (str)
 VALUES ("has a description given by");
-SET @fullLexItem_has_description = 7;
+SET @Str_has_description = 5 + 0x0A00000000000000;
 
 INSERT INTO Strings (str)
-VALUES (".Description=");
-SET @abbrLexItem_has_description = 8;
+VALUES (".description=");
+SET @Str_dotdescription = 6 + 0x0A00000000000000;
 
-INSERT INTO Strings (str)
+INSERT INTO Texts (str)
 VALUES ( -- TODO: write this description.
     "TODO: write this description.\n"
     -- "TODO: Write about standard of beginning relations with something like:\n",
     -- "Relation: Object (a String) forms a lexical item which references",
     -- "Subject (any Term)."
 );
-SET @description_has_description = 9;
+SET @Text_description_of_hasdescription = 3 + 0x0B00000000000000;
 
 
 
 -- insert the '.Description=' fundamental relation.
-INSERT INTO SimpleTerms (
-    full_lexical_item,
-    abbr_lexical_item,
-    description
-)
-VALUES (
-    @fullLexItem_has_description,
-    @abbrLexItem_has_description,
-    @description_has_description
-);
-SET @rel_has_description = 3;
+-- INSERT INTO SimpleTerms (
+--     full_lexical_item,
+--     abbr_lexical_item,
+--     description
+-- )
+-- VALUES (
+--     @Str_has_description,
+--     @Str_dotdescription,
+--     @Str_description_of_hasdescription
+-- );
+SET @Term_rel_hasdescription = 3 + 0x0200000000000000;
 
 
 -- types for reference:
@@ -213,24 +207,41 @@ SET @rel_has_description = 3;
 --     description
 -- )
 -- VALUES (
---     @fullLexItem_has_full_lex,
---     @abbrLexItem_has_full_lex,
---     @description_has_full_lex
+--     @Str_canbereferencedbyfulllex,
+--     @Str_dotfulllex,
+--     @Str_description_of_rel_fulllex
 -- );
--- SET @rel_has_full_lex = 1;
+-- SET @Term_rel_hasfulllex = 1;
 
+
+-- insert ralations about '.full lexical item' relation.
 INSERT INTO SemanticInputs (
-    subj_t, subj_id,
-    user_t, user_id,
-    rel_t, rel_id,
-    obj_t, obj_id,
+    subj_id,
+    user_id,
+    rel_id,
+    obj_id,
     rat_val, opt_data
 )
-VALUES (
-    2, @rel_has_full_lex,
-    0, @author_bot,
-    2, @rel_has_full_lex,
-    5, @fullLexItem_has_full_lex,
+VALUES
+(
+    @Term_rel_hasfulllex,
+    @author_bot,
+    @Term_rel_hasfulllex,
+    @Str_canbereferencedbyfulllex,
+    0x7FFFFFFF, NULL
+),
+(
+    @Term_rel_hasfulllex,
+    @author_bot,
+    @Term_rel_haslex,
+    @Str_dotfulllex,
+    0x7FFFFFFF, NULL
+),
+(
+    @Term_rel_hasfulllex,
+    @author_bot,
+    @Term_rel_hasdescription,
+    @Text_description_of_rel_fulllex,
     0x7FFFFFFF, NULL
 );
 
