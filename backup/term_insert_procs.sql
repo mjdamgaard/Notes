94a0c4ab3d395ceabcc38e34a79ae51e082468d5
mjdamgaard <43005772+mjdamgaard@users.noreply.github.com>
2023-04-04 16:51:12 +0200

    Add files via upload

--- backup/term_insert_procs.sql
+++ backup/term_insert_procs.sql
@@ -1,36 +1,13 @@
--- USE mydatabase;
 
 
-
-
-
+SELECT "term_inserts";
 
 
 DROP PROCEDURE insertOrFindCat;
-DROP PROCEDURE insertOrFindStd;
+DROP PROCEDURE insertOrFindETerm;
 DROP PROCEDURE insertOrFindRel;
 
-DROP PROCEDURE insertTxt;
-
-
-
--- DELETE FROM Sets;
--- DELETE FROM SemanticInputs;
---
--- DELETE FROM UserGroups;
--- DELETE FROM Users;
---
--- TRUNCATE TABLE Categories; -- slow..
--- DELETE FROM StandardTerms;
--- DELETE FROM Relations;
--- DELETE FROM KeywordStrings;
--- DELETE FROM SavedSets;
--- DELETE FROM Texts;
--- DELETE FROM Lists;
--- DELETE FROM Binaries;
---
--- DELETE FROM Creators;
-
+DROP PROCEDURE insertText;
 
 
 
@@ -39,79 +16,77 @@ DROP PROCEDURE insertTxt;
 
 DELIMITER //
 CREATE PROCEDURE insertOrFindCat (
-    IN catTitle TEXT,
-    IN superCatIDHex VARCHAR(16),
-    IN userIDHex VARCHAR(16),
-    OUT newIDHex VARCHAR(16),
-    OUT exitCode TINYINT -- 0 is successful insertion, 1 is successful find.
+    IN userCombID VARCHAR(17),
+    IN superCatCombID VARCHAR(17),
+    IN catTitle VARCHAR(255),
+    OUT newCombID VARCHAR(17),
+    OUT exitCode TINYINT
 )
 BEGIN
     DECLARE superCatID, userID, newID BIGINT UNSIGNED;
-    SET superCatID = CONV(superCatIDHex, 16, 10);
-    SET userID = CONV(userIDHex, 16, 10);
+    CALL getConvID (superCatCombID, superCatID);
+    CALL getConvID (userCombID, userID);
 
     SELECT id INTO newID
     FROM Categories
     WHERE (title = catTitle AND super_cat_id = superCatID);
     IF (newID IS NOT NULL) THEN
         SET exitCode = 1; -- find.
+    ELSEIF (NOT EXISTS (SELECT id FROM Categories WHERE id = superCatID)) THEN
+        SET newID = NULL;
+        SET exitCode = 2; -- super category doesn't exist.
     ELSE
-        IF (NOT EXISTS (SELECT id FROM Categories WHERE id = superCatID)) THEN
-            SET newID = NULL;
-            SET exitCode = 2; -- super category doesn't exist.
-        ELSE
-            INSERT INTO Categories (title, super_cat_id)
-            VALUES (catTitle, superCatID);
-            SELECT LAST_INSERT_ID() INTO newID;
-            -- NOTE: This procedure assumes that user_id is correct if not null.
-            IF (userID IS NOT NULL) THEN
-                INSERT INTO Creators (term_t, term_id, user_id)
-                VALUES ("c", newID, userID);
-            END IF;
-            SET exitCode = 0; -- insert.
+        -- TODO: Insert a check that the user is part of a set and with a
+        -- non-negative rating denoting that the user is allowed to insert.
+        INSERT INTO Categories (title, super_cat_id)
+        VALUES (catTitle, superCatID);
+        SELECT LAST_INSERT_ID() INTO newID;
+        IF (userID != 0) THEN
+            INSERT INTO Creators (term_t, term_id, user_id)
+            VALUES ("c", newID, userID);
         END IF;
+        SET exitCode = 0; -- insert.
     END IF;
-    SET newIDHex = HEX(newID);
+    SET newCombID = CONCAT('c', CONV(newID, 10, 16));
 END //
 DELIMITER ;
 
 
 
 DELIMITER //
-CREATE PROCEDURE insertOrFindStd (
-    IN stdTitle TEXT,
-    IN catIDHex VARCHAR(16),
-    IN userIDHex VARCHAR(16),
-    OUT newIDHex VARCHAR(16),
-    OUT exitCode TINYINT -- 0 is successful insertion, 1 is successful find.
+CREATE PROCEDURE insertOrFindETerm (
+    IN userCombID VARCHAR(17),
+    IN catCombID VARCHAR(17),
+    IN eTermTitle VARCHAR(255),
+    OUT newCombID VARCHAR(17),
+    OUT exitCode TINYINT
 )
 BEGIN
     DECLARE catID, userID, newID BIGINT UNSIGNED;
-    SET catID = CONV(catIDHex, 16, 10);
-    SET userID = CONV(userIDHex, 16, 10);
+    CALL getConvID (catCombID, catID);
+    CALL getConvID (userCombID, userID);
 
     SELECT id INTO newID
-    FROM StandardTerms
-    WHERE (title = stdTitle AND cat_id = catID);
+    FROM ElementaryTerms
+    WHERE (title = eTermTitle AND cat_id = catID);
     IF (newID IS NOT NULL) THEN
         SET exitCode = 1; -- find.
+    ELSEIF (NOT EXISTS (SELECT id FROM Categories WHERE id = catID)) THEN
+        SET newID = NULL;
+        SET exitCode = 2; -- category doesn't exist.
     ELSE
-        IF (NOT EXISTS (SELECT id FROM Categories WHERE id = catID)) THEN
-            SET newID = NULL;
-            SET exitCode = 2; -- category doesn't exist.
-        ELSE
-            INSERT INTO StandardTerms (title, cat_id)
-            VALUES (stdTitle, catID);
-            SELECT LAST_INSERT_ID() INTO newID;
-            -- NOTE: This procedure assumes that user_id is correct if not null.
-            IF (userID IS NOT NULL) THEN
-                INSERT INTO Creators (term_t, term_id, user_id)
-                VALUES ("s", newID, userID);
-            END IF;
-            SET exitCode = 0; -- insert.
+        -- TODO: Insert a check that the user is part of a set and with a
+        -- non-negative rating denoting that the user is allowed to insert.
+        INSERT INTO ElementaryTerms (title, cat_id)
+        VALUES (eTermTitle, catID);
+        SELECT LAST_INSERT_ID() INTO newID;
+        IF (userID != 0) THEN
+            INSERT INTO Creators (term_t, term_id, user_id)
+            VALUES ("e", newID, userID);
         END IF;
+        SET exitCode = 0; -- insert.
     END IF;
-    SET newIDHex = HEX(newID);
+    SET newCombID = CONCAT('e', CONV(newID, 10, 16));
 END //
 DELIMITER ;
 
@@ -120,39 +95,38 @@ DELIMITER ;
 
 DELIMITER //
 CREATE PROCEDURE insertOrFindRel (
-    IN objNoun TEXT,
-    IN subjCatIDHex VARCHAR(16),
-    IN userIDHex VARCHAR(16),
-    OUT newIDHex VARCHAR(16),
-    OUT exitCode TINYINT -- 0 is successful insertion, 1 is successful find.
+    IN userCombID VARCHAR(17),
+    IN subjCatCombID VARCHAR(17),
+    IN objNoun VARCHAR(255),
+    OUT newCombID VARCHAR(17),
+    OUT exitCode TINYINT
 )
 BEGIN
     DECLARE subjCatID, userID, newID BIGINT UNSIGNED;
-    SET subjCatID = CONV(subjCatIDHex, 16, 10);
-    SET userID = CONV(userIDHex, 16, 10);
+    CALL getConvID (subjCatCombID, subjCatID);
+    CALL getConvID (userCombID, userID);
 
     SELECT id INTO newID
     FROM Relations
     WHERE (obj_noun = objNoun AND subj_cat_id = subjCatID);
     IF (newID IS NOT NULL) THEN
         SET exitCode = 1; -- find.
+    ELSEIF (NOT EXISTS (SELECT id FROM Categories WHERE id = subjCatID)) THEN
+        SET newID = NULL;
+        SET exitCode = 2; -- subject category doesn't exist.
     ELSE
-        IF (NOT EXISTS (SELECT id FROM Categories WHERE id = subjCatID)) THEN
-            SET newID = NULL;
-            SET exitCode = 2; -- subject category doesn't exist.
-        ELSE
-            INSERT INTO Relations (obj_noun, subj_cat_id)
-            VALUES (objNoun, subjCatID);
-            SELECT LAST_INSERT_ID() INTO newID;
-            -- NOTE: This procedure assumes that user_id is correct if not null.
-            IF (userID IS NOT NULL) THEN
-                INSERT INTO Creators (term_t, term_id, user_id)
-                VALUES ("r", newID, userID);
-            END IF;
-            SET exitCode = 0; -- insert.
+        -- TODO: Insert a check that the user is part of a set and with a
+        -- non-negative rating denoting that the user is allowed to insert.
+        INSERT INTO Relations (obj_noun, subj_cat_id)
+        VALUES (objNoun, subjCatID);
+        SELECT LAST_INSERT_ID() INTO newID;
+        IF (userID != 0) THEN
+            INSERT INTO Creators (term_t, term_id, user_id)
+            VALUES ("r", newID, userID);
         END IF;
+        SET exitCode = 0; -- insert.
     END IF;
-    SET newIDHex = HEX(newID);
+    SET newCombID = CONCAT('r', CONV(newID, 10, 16));
 END //
 DELIMITER ;
 
@@ -164,24 +138,33 @@ DELIMITER ;
 
 
 DELIMITER //
-CREATE PROCEDURE insertTxt (
+CREATE PROCEDURE insertText (
+    IN userCombID VARCHAR(17),
     IN textStr TEXT,
-    IN userIDHex VARCHAR(16),
-    OUT newIDHex VARCHAR(16),
+    OUT newCombID VARCHAR(17),
     OUT exitCode TINYINT -- 0 is successful insertion.
 )
 BEGIN
     DECLARE userID, newID BIGINT UNSIGNED;
-    SET userID = CONV(userIDHex, 16, 10);
+    CALL getConvID (userCombID, userID);
 
-    INSERT INTO Texts (str) VALUES (textStr);
-    SELECT LAST_INSERT_ID() INTO newID;
-    IF (userID IS NOT NULL) THEN
-        -- NOTE: This procedure assumes that user_id is correct if not null.
-        INSERT INTO Creators (term_t, term_id, user_id)
-        VALUES ("t", newID, userID);
+    SELECT id INTO newID
+    FROM Texts
+    WHERE (str = textStr);
+    IF (newID IS NOT NULL) THEN
+        SET exitCode = 1; -- find.
+    ELSE
+        -- TODO: Insert a check that the user is part of a set and with a
+        -- non-negative rating denoting that the user is allowed to insert.
+        INSERT INTO Texts (str)
+        VALUES (textStr);
+        SELECT LAST_INSERT_ID() INTO newID;
+        IF (userID != 0) THEN
+            INSERT INTO Creators (term_t, term_id, user_id)
+            VALUES ("t", newID, userID);
+        END IF;
+        SET exitCode = 0; -- insert.
     END IF;
-    SET exitCode = 0; -- insert.
-    SET newIDHex = HEX(newID);
+    SET newCombID = CONCAT('t', CONV(newID, 10, 16));
 END //
 DELIMITER ;
