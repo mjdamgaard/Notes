63622ad6374df7a86bf6a384316c3f044d54ee91
Mads Juul Damgaard <43005772+mjdamgaard@users.noreply.github.com>
2023-07-18 14:41:14 +0200

    Add files via upload

--- backup/initial_inserts.sql
+++ backup/initial_inserts.sql
@@ -46,24 +46,24 @@ VALUES
     -- Property type:
     (1, NULL, "Property", 19),
     -- Property template:
-    (3, NULL, "{<Title>}, one-to-<Quantity word (e.g. 'one,' 'few,' or 'many')>", 20),
+    (3, 19, "{<Title>}, one-to-<Quantity word (e.g. 'one,' 'few,' or 'many')>", 20),
     -- Property category template:
-    (3, NULL, "<Property> of <Entity>", 21),
+    (3, 2, "<Property> of <Entity>", 21),
     -- Subcategory template:
-    (3, NULL, "{<Title>} (<Supercategory>)", 22),
+    (3, 2, "{<Title>} (<Supercategory>)", 22),
     -- Adjective subcategory template:
-    (3, NULL, "<Adjective phrase> <Category>", 23);
+    (3, 2, "<Adjective phrase> <Category>", 23);
 
 
 
 /* More inserts for testing */
 
 CALL insertOrFindEntity(9, 2, 0, "Music"); -- id: 24
-CALL insertOrFindEntity(9, 2, 22, "Rock|#23"); -- id: 25
-CALL insertOrFindEntity(9, 2, 22, "Jazz|#23"); -- id: 26
+CALL insertOrFindEntity(9, 2, 22, "Rock|#24"); -- id: 25
+CALL insertOrFindEntity(9, 2, 22, "Jazz|#24"); -- id: 26
 
 CALL insertOrFindEntity(9, 1, 0, "Movie"); -- id: 27
-CALL insertOrFindEntity(9, 3, 0, "{<Title>}, <Year>"); -- id: 28
+CALL insertOrFindEntity(9, 3, 27, "{<Title>}, <Year>"); -- id: 28
 CALL insertOrFindEntity(9, 27, 28,
     "The Lord of the Rings: The Fellowship of the Ring|2001"
 ); -- id: 29
@@ -83,7 +83,7 @@ CALL insertOrFindEntity(9, 2, 22, "Mathematics|#31"); -- id: 36
 -- rate some statements.
 
 CALL insertOrFindEntity(9, 19, 20, "Subcategories|many"); -- id: 37
-CALL insertOrFindEntity(9, 2, 21, "#37|#1"); -- id: 38
+CALL insertOrFindEntity(9, 2, 21, "#37|#10"); -- id: 38
 CALL inputOrChangeRating(9, 38, 32, CONV("FFFF", 16, 10), "00");
 CALL inputOrChangeRating(9, 38, 31, CONV("F000", 16, 10), "00");
 CALL inputOrChangeRating(9, 38, 24, CONV("F000", 16, 10), "00");
@@ -98,11 +98,14 @@ CALL inputOrChangeRating(9, 40, 26, CONV("E000", 16, 10), "00");
 CALL inputOrChangeRating(9, 40, 39, CONV("0001", 16, 10), "00");
 CALL inputOrChangeRating(9, 40, 33, CONV("F000", 16, 10), "00");
 
-CALL insertOrFindEntity(9, 2, 21, "#37|#31"); -- id: 41
-CALL inputOrChangeRating(9, 41, 32, CONV("F000", 16, 10), "00");
+CALL insertOrFindEntity(9, 2, 21, "#37|#32"); -- id: 41
+CALL inputOrChangeRating(9, 41, 33, CONV("F000", 16, 10), "00");
+CALL inputOrChangeRating(9, 41, 34, CONV("F010", 16, 10), "00");
+CALL inputOrChangeRating(9, 41, 35, CONV("F050", 16, 10), "00");
+CALL inputOrChangeRating(9, 41, 36, CONV("F100", 16, 10), "00");
 
 CALL inputOrChangeRating(9, 10, 25, CONV("A000", 16, 10), "00");
-CALL inputOrChangeRating(9, 10, 37, CONV("A400", 16, 10), "00");
+CALL inputOrChangeRating(9, 10, 31, CONV("A400", 16, 10), "00");
 CALL inputOrChangeRating(9, 10, 29, CONV("9003", 16, 10), "00");
 CALL inputOrChangeRating(9, 10, 30, CONV("9002", 16, 10), "00");
 
@@ -120,25 +123,26 @@ CALL inputOrChangeRating(9, 45, 33, CONV("F000", 16, 10), "00");
 CALL insertOrFindEntity(9, 2, 21, "#42|#33"); -- id: 46
 CALL inputOrChangeRating(9, 46, 24, CONV("FF00", 16, 10), "00");
 
+CALL insertOrFindEntity(9, 19, 20, "Supercategories|many"); -- id: 47
 
-CALL insertOrFindEntity(9, 2, 23, "good"); -- id: 47
-CALL insertOrFindEntity(9, 2, 23, "funny"); -- id: 48
-CALL insertOrFindEntity(9, 2, 23, "scary"); -- id: 49
-CALL insertOrFindEntity(9, 2, 23, "iconic"); -- id: 50
+CALL insertOrFindEntity(9, 2, 23, "good"); -- id: 48
+CALL insertOrFindEntity(9, 2, 23, "funny"); -- id: 49
+CALL insertOrFindEntity(9, 2, 23, "scary"); -- id: 50
+CALL insertOrFindEntity(9, 2, 23, "iconic"); -- id: 51
 
 -- TODO: Redo the last part of this file as well:
 
--- CALL insertOrFindEntity(9, 'p', 12, "Relevant ratings for derived entities|#1"); -- id: 50
+-- CALL insertOrFindEntity(9, 'p', 12, "Relevant categories for derived entities|#1"); -- id: 50
 -- CALL inputOrChangeRating(9, 50, 46, CONV("F000", 16, 10), "00");
 -- CALL inputOrChangeRating(9, 50, 40, CONV("F000", 16, 10), "00");
 --
--- CALL insertOrFindEntity(9, 'p', 12, "Relevant ratings for derived entities|#27"); -- id: 51
+-- CALL insertOrFindEntity(9, 'p', 12, "Relevant categories for derived entities|#27"); -- id: 51
 -- CALL inputOrChangeRating(9, 51, 46, CONV("F000", 16, 10), "00");
 -- CALL inputOrChangeRating(9, 51, 47, CONV("E100", 16, 10), "00");
 -- CALL inputOrChangeRating(9, 51, 48, CONV("E000", 16, 10), "00");
 --
 --
--- CALL insertOrFindEntity(9, 'p', 12, "Relevant ratings|#28"); -- id: 52
+-- CALL insertOrFindEntity(9, 'p', 12, "Relevant categories|#28"); -- id: 52
 -- CALL inputOrChangeRating(9, 52, 46, CONV("F000", 16, 10), "00");
 -- CALL inputOrChangeRating(9, 52, 47, CONV("E100", 16, 10), "00");
 -- CALL inputOrChangeRating(9, 52, 48, CONV("EAAA", 16, 10), "00");
